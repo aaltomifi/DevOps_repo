5045abac8eeef95d0e071a3c415beb0a6d6dc907
cryptic404 <willestrengell@hotmail.com>
2020-09-07 15:44:36 +0300

    Update Devops.java

--- Devops.java
+++ Devops.java
@@ -15,7 +15,7 @@ public class Devops {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Good Morning Vietnam");
     }
     
     public void HelloWorld(){
